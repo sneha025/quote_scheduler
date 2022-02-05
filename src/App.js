2b00910adc880c550f922a2b22cd61b53761e04b
sneha025 <simaurya96@gmail.com>
2022-02-05 22:16:34 +0530

    added fetch in node

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 function App() {
-  const [quote,setQuote]=("")
+  // const [quote, setQuote] = useState("");
+
   //TODO: call netlify function which will return the value
   //TODO: useEffect will listen if there is any chnage happening in current quote state
 
   return (
     <React.Fragment>
-      <h1>Hello there</h1>
+      <h1>Hello there!</h1>
     </React.Fragment>
   );
 }
